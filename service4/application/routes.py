e5fca9d97c83b67c368165b329975636f6155eed
dond45 <dondre.ty20@hotmail.com>
2021-12-03 19:06:21 +0000

    updated service 4 folder, app added database sqlite and updated dockerfile

---
+++ service4/application/routes.py
@@ -0,0 +1,16 @@
+from application import app
+from flask import Flask, request, Response
+import random
+
+@app.route('/service4/prize', methods=['POST'])
+def name():
+    name = request.data.decode("utf-8")
+    if name == 'peter' and number == 1:
+        prize = 'teddy'
+    elif name == 'paul' and number == 2:
+        prize = 'chocolate'
+    elif name == 'john' and number == 3:
+        prize = 'sweets'
+    else:
+        prize = "sorry! names and numbers did not match, you have won nothing! "
+    return Response(prize, mimetype='text/plain')
