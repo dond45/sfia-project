00dd295f9400fb6b90fd1ff21263adf5a64c9ea6
dond45 <dondre.ty20@hotmail.com>
2021-12-04 21:10:26 +0000

    service3 updated

--- service3/application/routes.py
+++ service3/application/routes.py
@@ -3,7 +3,7 @@ from flask import Flask, request, Response
 import requests
 import random
 
-@app.route('/service3/number', methods=['GET'])
+@app.route('/service3/number_gen', methods=['GET'])
 def number():
     Number = number[randint(0,2)]
     return Response(Number, mimetype="text/plain")
