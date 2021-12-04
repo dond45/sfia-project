0b63b530b2744716a4dcf462d79e6694f6174bbc
dond45 <dondre.ty20@hotmail.com>
2021-12-04 21:10:02 +0000

    service2 updated

--- service2/application/routes.py
+++ service2/application/routes.py
@@ -3,9 +3,9 @@ from flask import Flask, request, Response
 import requests
 import random
 
-@app.route('/service2/name', methods=['GET'])
-def name():
-    name = ["peter", "paul", "john"]
-    Name = name[randint(0,2)]
-    return Response(Name, mimetype="text/plain")
+@app.route('/service2/name_gen', methods=['GET'])
+def name_gen():
+    name_gen = ["peter", "paul", "john"]
+    name_gen = name[randint(0,2)]
+    return Response(name, mimetype="text/plain")
     
